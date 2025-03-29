b089b6dcaf5b30c0f5174421ba5472c30d9dfefc
DragonSCP <167790175+DragonSCPOFICIAL@users.noreply.github.com>
2025-03-29 13:11:20 -0300

    Create world2_missions.lua

---
+++ world2_missions.lua
@@ -0,0 +1,223 @@
+-- world2_missions.lua
+-- Tabela com os dados das missões do segundo mundo
+local World2Missions = {
+    {
+        minLevel = 700, maxLevel = 724,
+        Mon = "Raider",
+        LevelQuest = 1,
+        NameQuest = "Area1Quest",
+        NameMon = "Raider",
+        CFrameQuest = CFrame.new(-429.543518, 71.7699966, 1836.18188, -0.22495985, 0, -0.974368095, 0, 1, 0, 0.974368095, 0, -0.22495985),
+        CFrameMon = CFrame.new(-728.3267211914062, 52.779319763183594, 2345.7705078125)
+    },
+    {
+        minLevel = 725, maxLevel = 774,
+        Mon = "Mercenary",
+        LevelQuest = 2,
+        NameQuest = "Area1Quest",
+        NameMon = "Mercenary",
+        CFrameQuest = CFrame.new(-429.543518, 71.7699966, 1836.18188, -0.22495985, 0, -0.974368095, 0, 1, 0, 0.974368095, 0, -0.22495985),
+        CFrameMon = CFrame.new(-1004.3244018554688, 80.15886688232422, 1424.619384765625)
+    },
+    {
+        minLevel = 775, maxLevel = 799,
+        Mon = "Swan Pirate",
+        LevelQuest = 1,
+        NameQuest = "Area2Quest",
+        NameMon = "Swan Pirate",
+        CFrameQuest = CFrame.new(638.43811, 71.769989, 918.282898, 0.139203906, 0, 0.99026376, 0, 1, 0, -0.99026376, 0, 0.139203906),
+        CFrameMon = CFrame.new(1068.664306640625, 137.61428833007812, 1322.1060791015625)
+    },
+    {
+        minLevel = 800, maxLevel = 874,
+        Mon = "Factory Staff",
+        LevelQuest = 2,
+        NameQuest = "Area2Quest",
+        NameMon = "Factory Staff",
+        CFrameQuest = CFrame.new(632.698608, 73.1055908, 918.666321, -0.0319722369, 8.96074881e-10, -0.999488771, 1.36326533e-10, 1, 8.92172336e-10, 0.999488771, -1.07732087e-10, -0.0319722369),
+        CFrameMon = CFrame.new(73.07867431640625, 81.86344146728516, -27.470672607421875)
+    },
+    {
+        minLevel = 875, maxLevel = 899,
+        Mon = "Marine Lieutenant",
+        LevelQuest = 1,
+        NameQuest = "MarineQuest3",
+        NameMon = "Marine Lieutenant",
+        CFrameQuest = CFrame.new(-2440.79639, 71.7140732, -3216.06812, 0.866007268, 0, 0.500031412, 0, 1, 0, -0.500031412, 0, 0.866007268),
+        CFrameMon = CFrame.new(-2821.372314453125, 75.89727783203125, -3070.089111328125)
+    },
+    {
+        minLevel = 900, maxLevel = 949,
+        Mon = "Marine Captain",
+        LevelQuest = 2,
+        NameQuest = "MarineQuest3",
+        NameMon = "Marine Captain",
+        CFrameQuest = CFrame.new(-2440.79639, 71.7140732, -3216.06812, 0.866007268, 0, 0.500031412, 0, 1, 0, -0.500031412, 0, 0.866007268),
+        CFrameMon = CFrame.new(-1861.2310791015625, 80.17658233642578, -3254.697509765625)
+    },
+    {
+        minLevel = 950, maxLevel = 974,
+        Mon = "Zombie",
+        LevelQuest = 1,
+        NameQuest = "ZombieQuest",
+        NameMon = "Zombie",
+        CFrameQuest = CFrame.new(-5497.06152, 47.5923004, -795.237061, -0.29242146, 0, -0.95628953, 0, 1, 0, 0.95628953, 0, -0.29242146),
+        CFrameMon = CFrame.new(-5657.77685546875, 78.96973419189453, -928.68701171875)
+    },
+    {
+        minLevel = 975, maxLevel = 999,
+        Mon = "Vampire",
+        LevelQuest = 2,
+        NameQuest = "ZombieQuest",
+        NameMon = "Vampire",
+        CFrameQuest = CFrame.new(-5497.06152, 47.5923004, -795.237061, -0.29242146, 0, -0.95628953, 0, 1, 0, 0.95628953, 0, -0.29242146),
+        CFrameMon = CFrame.new(-6037.66796875, 32.18463897705078, -1340.6597900390625)
+    },
+    {
+        minLevel = 1000, maxLevel = 1049,
+        Mon = "Snow Trooper",
+        LevelQuest = 1,
+        NameQuest = "SnowMountainQuest",
+        NameMon = "Snow Trooper",
+        CFrameQuest = CFrame.new(609.858826, 400.119904, -5372.25928, -0.374604106, 0, 0.92718488, 0, 1, 0, -0.92718488, 0, -0.374604106),
+        CFrameMon = CFrame.new(549.1473388671875, 427.3870544433594, -5563.69873046875)
+    },
+    {
+        minLevel = 1050, maxLevel = 1099,
+        Mon = "Winter Warrior",
+        LevelQuest = 2,
+        NameQuest = "SnowMountainQuest",
+        NameMon = "Winter Warrior",
+        CFrameQuest = CFrame.new(609.858826, 400.119904, -5372.25928, -0.374604106, 0, 0.92718488, 0, 1, 0, -0.92718488, 0, -0.374604106),
+        CFrameMon = CFrame.new(1142.7451171875, 475.6398010253906, -5199.41650390625)
+    },
+    {
+        minLevel = 1100, maxLevel = 1124,
+        Mon = "Lab Subordinate",
+        LevelQuest = 1,
+        NameQuest = "IceSideQuest",
+        NameMon = "Lab Subordinate",
+        CFrameQuest = CFrame.new(-6064.06885, 15.2422857, -4902.97852, 0.453972578, 0, -0.891015649, 0, 1, 0, 0.891015649, 0, 0.453972578),
+        CFrameMon = CFrame.new(-5707.4716796875, 15.951709747314453, -4513.39208984375)
+    },
+    {
+        minLevel = 1125, maxLevel = 1174,
+        Mon = "Horned Warrior",
+        LevelQuest = 2,
+        NameQuest = "IceSideQuest",
+        NameMon = "Horned Warrior",
+        CFrameQuest = CFrame.new(-6064.06885, 15.2422857, -4902.97852, 0.453972578, 0, -0.891015649, 0, 1, 0, 0.891015649, 0, 0.453972578),
+        CFrameMon = CFrame.new(-6341.36669921875, 15.951770782470703, -5723.162109375)
+    },
+    {
+        minLevel = 1175, maxLevel = 1199,
+        Mon = "Magma Ninja",
+        LevelQuest = 1,
+        NameQuest = "FireSideQuest",
+        NameMon = "Magma Ninja",
+        CFrameQuest = CFrame.new(-5428.03174, 15.0622921, -5299.43457, -0.882952213, 0, 0.469463557, 0, 1, 0, -0.469463557, 0, -0.882952213),
+        CFrameMon = CFrame.new(-5449.6728515625, 76.65874481201172, -5808.20068359375)
+    },
+    {
+        minLevel = 1200, maxLevel = 1249,
+        Mon = "Lava Pirate",
+        LevelQuest = 2,
+        NameQuest = "FireSideQuest",
+        NameMon = "Lava Pirate",
+        CFrameQuest = CFrame.new(-5428.03174, 15.0622921, -5299.43457, -0.882952213, 0, 0.469463557, 0, 1, 0, -0.469463557, 0, -0.882952213),
+        CFrameMon = CFrame.new(-5213.33154296875, 49.73788070678711, -4701.451171875)
+    },
+    {
+        minLevel = 1250, maxLevel = 1274,
+        Mon = "Ship Deckhand",
+        LevelQuest = 1,
+        NameQuest = "ShipQuest1",
+        NameMon = "Ship Deckhand",
+        CFrameQuest = CFrame.new(1037.80127, 125.092171, 32911.6016),
+        CFrameMon = CFrame.new(1212.0111083984375, 150.79205322265625, 33059.24609375),
+        requestEntranceCoord = Vector3.new(923.21252441406, 126.9760055542, 32852.83203125)
+    },
+    {
+        minLevel = 1275, maxLevel = 1299,
+        Mon = "Ship Engineer",
+        LevelQuest = 2,
+        NameQuest = "ShipQuest1",
+        NameMon = "Ship Engineer",
+        CFrameQuest = CFrame.new(1037.80127, 125.092171, 32911.6016),
+        CFrameMon = CFrame.new(919.4786376953125, 43.54401397705078, 32779.96875),
+        requestEntranceCoord = Vector3.new(923.21252441406, 126.9760055542, 32852.83203125)
+    },
+    {
+        minLevel = 1300, maxLevel = 1324,
+        Mon = "Ship Steward",
+        LevelQuest = 1,
+        NameQuest = "ShipQuest2",
+        NameMon = "Ship Steward",
+        CFrameQuest = CFrame.new(968.80957, 125.092171, 33244.125),
+        CFrameMon = CFrame.new(919.4385375976562, 129.55599975585938, 33436.03515625),
+        requestEntranceCoord = Vector3.new(923.21252441406, 126.9760055542, 32852.83203125)
+    },
+    {
+        minLevel = 1325, maxLevel = 1349,
+        Mon = "Ship Officer",
+        LevelQuest = 2,
+        NameQuest = "ShipQuest2",
+        NameMon = "Ship Officer",
+        CFrameQuest = CFrame.new(968.80957, 125.092171, 33244.125),
+        CFrameMon = CFrame.new(1036.0179443359375, 181.4390411376953, 33315.7265625),
+        requestEntranceCoord = Vector3.new(923.21252441406, 126.9760055542, 32852.83203125)
+    },
+    {
+        minLevel = 1350, maxLevel = 1374,
+        Mon = "Arctic Warrior",
+        LevelQuest = 1,
+        NameQuest = "FrostQuest",
+        NameMon = "Arctic Warrior",
+        CFrameQuest = CFrame.new(5667.6582, 26.7997818, -6486.08984, -0.933587909, 0, -0.358349502, 0, 1, 0, 0.358349502, 0, -0.933587909),
+        CFrameMon = CFrame.new(5966.24609375, 62.97002029418945, -6179.3828125),
+        requestEntranceCoord = Vector3.new(-6508.5581054688, 5000.034996032715, -132.83953857422)
+    },
+    {
+        minLevel = 1375, maxLevel = 1424,
+        Mon = "Snow Lurker",
+        LevelQuest = 2,
+        NameQuest = "FrostQuest",
+        NameMon = "Snow Lurker",
+        CFrameQuest = CFrame.new(5667.6582, 26.7997818, -6486.08984, -0.933587909, 0, -0.358349502, 0, 1, 0, 0.358349502, 0, -0.933587909),
+        CFrameMon = CFrame.new(5407.07373046875, 69.19437408447266, -6880.88037109375)
+    },
+    {
+        minLevel = 1425, maxLevel = 1449,
+        Mon = "Sea Soldier",
+        LevelQuest = 1,
+        NameQuest = "ForgottenQuest",
+        NameMon = "Sea Soldier",
+        CFrameQuest = CFrame.new(-3054.44458, 235.544281, -10142.8193, 0.990270376, 0, -0.13915664, 0, 1, 0, 0.13915664, 0, 0.990270376),
+        CFrameMon = CFrame.new(-3028.2236328125, 64.67451477050781, -9775.4267578125)
+    },
+    {
+        minLevel = 1450, maxLevel = math.huge, -- Nível máximo para o segundo mundo
+        Mon = "Water Fighter",
+        LevelQuest = 2,
+        NameQuest = "ForgottenQuest",
+        NameMon = "Water Fighter",
+        CFrameQuest = CFrame.new(-3054.44458, 235.544281, -10142.8193, 0.990270376, 0, -0.13915664, 0, 1, 0, 0.13915664, 0, 0.990270376),
+        CFrameMon = CFrame.new(-3352.9013671875, 285.01556396484375, -10534.841796875)
+    }
+}
+
+-- Função para obter a missão correspondente ao nível atual do jogador
+local function getMissionByLevel(MyLevel)
+    for _, mission in ipairs(World2Missions) do
+        if MyLevel >= mission.minLevel and MyLevel <= mission.maxLevel then
+            return mission
+        end
+    end
+    return nil -- Caso não haja missão definida para o nível
+end
+
+-- Retornando a tabela e a função para uso externo
+return {
+    missions = World2Missions,
+    getMissionByLevel = getMissionByLevel
+}
